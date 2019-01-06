8033d6a924368a409446128db9e3307a4568a6cb
Khemissi Eddin <khemissihoussemeddin@khemissis-mac.local>
2019-01-06 06:17:47 -0800

    Initial Commit

---
+++ BestMovie/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BestMovie
+//
+//  Created by Khemissi Houssem Eddin on 06/01/2019.
+//  Copyright © 2019 Nizar Elhraiech. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
